6137c1230e8773155e605a8736dc06d7e3a56066
Daithi Hearn <daithi.hearn@gmail.com>
2020-06-11 17:14:11 +0200

    Replacing Quizmaster with Cards

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -11,7 +11,7 @@ function NavBar(props) {
   return (
     <nav className="navbar navbar-dark bg-primary fixed-top">
       <Link className="navbar-brand" to="/">
-        Quizmaster
+        Cards
       </Link>
       {
         !auth0Client.isAuthenticated() &&
