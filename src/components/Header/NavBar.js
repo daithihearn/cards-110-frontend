5d439fb5448cd8de00ba06d64b1c4fc047ff75e7
Daithi Hearn <dahearn@deloitte.ie>
2022-02-19 10:03:16 +0100

    Avatar Editor Adding editor for avatar

--- src/components/Header/NavBar.js
+++ src/components/Header/NavBar.js
@@ -3,8 +3,9 @@ import { Link } from "react-router-dom"
 import profileService from '../../services/ProfileService'
 import { useAuth0 } from '@auth0/auth0-react'
 import parseError from '../../utils/ErrorUtils'
+import AvatarEditor from 'react-avatar-editor'
 
-import { Col, Modal, ModalBody, ModalHeader, ModalFooter, Button, Form, FormGroup, FormText, Label, Input } from 'reactstrap'
+import { Col, Modal, ModalBody, ModalHeader, ModalFooter, Button, Form, FormGroup, FormText, Label, Input, ButtonGroup } from 'reactstrap'
 import { useDispatch, useSelector } from 'react-redux'
 
 const NavBar = () => {
@@ -18,10 +19,43 @@ const NavBar = () => {
     if (!myProfile) { return null }
 
     const [modalUpdateProfileOpen, updateModalUpdateProfileOpen] = useState(false)
+    const [selectedImage, updateSelectedImage] = useState(myProfile.picture)
+    const [scale, updateScale] = useState(1.2)
+    const [editor, setEditorRef] = useState(null)
 
     const signOut = () => {
         logout({ returnTo: window.location.origin })
         clearGame()
+    }
+
+    const handleScale = e => {
+        const newScale = parseFloat(e.target.value)
+        updateScale(newScale)
+    }
+
+    const handleSave = () => {
+        const canvasScaled = editor.getImageScaledToCanvas()
+        const croppedImg = canvasScaled.toDataURL()
+
+        getAccessTokenSilently().then(accessToken => {
+
+            profileService.updateProfile({ name: myProfile.name, picture: croppedImg, forceUpdate: true }, accessToken).then(profile => {
+                dispatch({
+                    type: 'myProfile/update', payload: {
+                        id: myProfile.id,
+                        name: myProfile.name,
+                        picture: profile.data.picture,
+                        isPlayer: myProfile.isPlayer,
+                        isAdmin: myProfile.isAdmin,
+                        accessToken: accessToken
+                    }
+                })
+            })
+            updateModalUpdateProfileOpen(false)
+
+        }).catch(error => {
+            dispatch({ type: 'snackbar/message', payload: { type: 'error', message: parseError(error) } })
+        })
     }
 
     const clearGame = () => {
@@ -42,33 +76,15 @@ const NavBar = () => {
 
         if (file.type.includes("image/")) {
 
-            getAccessTokenSilently().then(accessToken => {
+            let reader = new FileReader()
+
+            reader.onloadend = () => {
+                updateSelectedImage(reader.result)
+            }
+
+            reader.readAsDataURL(file)
 
 
-                let reader = new FileReader();
-
-                reader.onloadend = () => {
-
-                    profileService.updateProfile({ name: myProfile.name, picture: reader.result, forceUpdate: true }, accessToken).then(profile => {
-                        dispatch({
-                            type: 'myProfile/update', payload: {
-                                id: myProfile.id,
-                                name: myProfile.name,
-                                picture: profile.data.picture,
-                                isPlayer: myProfile.isPlayer,
-                                isAdmin: myProfile.isAdmin,
-                                accessToken: accessToken
-                            }
-                        })
-                    })
-                    updateModalUpdateProfileOpen(false)
-                }
-
-                reader.readAsDataURL(file);
-
-            }).catch(error => {
-                dispatch({ type: 'snackbar/message', payload: { type: 'error', message: parseError(error) } })
-            })
         } else {
             dispatch({ type: 'snackbar/message', payload: { type: 'error', message: "Invalid File Type" } })
         }
@@ -86,23 +102,53 @@ const NavBar = () => {
 
                     <Modal fade toggle={handleCloseUpdateProfileModal} isOpen={modalUpdateProfileOpen}>
                         <ModalBody>
+
                             <Form>
+                                <FormGroup row>
+                                    <AvatarEditor
+                                        ref={setEditorRef}
+                                        image={selectedImage}
+                                        width={250}
+                                        height={250}
+                                        border={50}
+                                        color={[255, 255, 255, 0.6]} // RGBA
+                                        scale={scale}
+                                        rotate={0}
+                                    />
+                                </FormGroup>
                                 <FormGroup row>
                                     <Label for="newAvatar" sm={2}>Avatar</Label>
                                     <Col sm={10}>
-                                        <Input type="file" name="newAvatar" id="newAvatar" onChange={handleNewAvatarSelection} multiple={false} />
+                                        <Input type="file" accept="image/*" name="newAvatar" id="newAvatar" onChange={handleNewAvatarSelection} multiple={false} />
                                         <FormText color="muted">
                                             Please choose your new avatar
-                                        </FormText>
+                                            </FormText>
                                     </Col>
+                                </FormGroup>
+                                <FormGroup row>
+                                    <Label for="scale">Zoom</Label>
+                                    <Input
+                                        name="scale"
+                                        type="range"
+                                        onChange={handleScale}
+                                        min="1"
+                                        max="2"
+                                        step="0.01"
+                                        defaultValue="1.2"
+                                    />
                                 </FormGroup>
                             </Form>
                         </ModalBody>
 
                         <ModalFooter>
-                            <Button color="secondary" onClick={handleCloseUpdateProfileModal}>
-                                Cancel
-                            </Button>
+                            <ButtonGroup size="lg">
+                                <Button color="secondary" onClick={handleCloseUpdateProfileModal}>
+                                    Cancel
+                                </Button>
+                                <Button color="primary" onClick={handleSave}>
+                                    Save
+                                </Button>
+                            </ButtonGroup>
                         </ModalFooter>
                     </Modal>
                 </div>
