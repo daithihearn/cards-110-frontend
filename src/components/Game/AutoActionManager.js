b4e27f64b72a46d2c22ffe530fbdbdfc0a1cf6be
Daithi Hearn <dahearn@deloitte.ie>
2022-02-26 01:59:36 +0100

    Autoplay Play lowest card if best trump lead Pass if in the bunker

--- src/components/Game/AutoActionManager.js
+++ src/components/Game/AutoActionManager.js
@@ -2,10 +2,31 @@ import { useState } from 'react'
 import GameService from '../../services/GameService'
 import parseError from '../../utils/ErrorUtils'
 import { useAuth0 } from '@auth0/auth0-react'
+import cards from '../../model/Cards'
 
 import { useDispatch } from 'react-redux'
 import sha1 from 'crypto-js/sha1'
 import { triggerBounceMessage } from '../../constants'
+
+const bestCardLead = (round) => {
+    let trumpCards = cards.filter(card => card.suit === round.suit || card.suit === "WILD")
+
+    // Remove played trump cards
+    round.completedHands.forEach(hand => {
+        for (let i in hand.playedCards) {
+            const card = hand.playedCards[i]
+            if (card.indexOf(round.suit) > -1 || card === "JOKER" || card === "ACE_HEARTS") {
+                trumpCards = trumpCards.filter((c) => card !== c.name)
+            }
+        }
+    })
+
+    // Sort Descending
+    trumpCards.sort((a, b) => b.value - a.value)
+
+    return round.currentHand.leadOut === trumpCards[0].name
+
+}
 
 const AutoActionManager = (props) => {
 
@@ -15,71 +36,110 @@ const AutoActionManager = (props) => {
 
     const [previousGameHash, updatePreviousGameHash] = useState("")
 
-    getAccessTokenSilently().then(accessToken => {
+    const game = props.game
+    const orderedCards = props.orderedCards
+    if (!game || !game.status || !orderedCards) {
+        return null
+    }
 
-        const game = props.game
-        const orderedCards = props.orderedCards
-        if (!game || !game.status || !orderedCards) {
-            return null
-        }
+    const autoPlayCards = orderedCards.filter(card => card.autoplay && card.selected)
 
-        const autoPlayCards = orderedCards.filter(card => card.autoplay && card.selected)
-
-        const deal = () => {
+    const deal = () => {
+        getAccessTokenSilently().then(accessToken => {
             GameService.deal(game.id, accessToken).catch(error => {
                 if (error.message === triggerBounceMessage) { return }
                 dispatch({ type: 'snackbar/message', payload: { type: 'error', message: parseError(error) } })
-
             })
-        }
-        const playCard = (card) => {
+        }).catch(error => {
+            dispatch({ type: 'snackbar/message', payload: { type: 'error', message: parseError(error) } })
+        })
+    }
+    const playCard = (card) => {
+        getAccessTokenSilently().then(accessToken => {
             GameService.playCard(game.id, card, accessToken).catch(error => {
                 if (error.message === triggerBounceMessage) { return }
                 dispatch({ type: 'snackbar/message', payload: { type: 'error', message: parseError(error) } })
             })
-        }
+        }).catch(error => {
+            dispatch({ type: 'snackbar/message', payload: { type: 'error', message: parseError(error) } })
+        })
+    }
 
-        const buyCards = (cards) => {
-            GameService.buyCards(game.id, cards, accessToken).then(response => {
+    const call = (callAmount) => {
+        getAccessTokenSilently().then(accessToken => {
+            GameService.call(game.id, callAmount, accessToken).catch(error => {
+                if (error.message === triggerBounceMessage) { return }
+                dispatch({ type: 'snackbar/message', payload: { type: 'error', message: parseError(error) } })
+            })
+        }).catch(error => {
+            dispatch({ type: 'snackbar/message', payload: { type: 'error', message: parseError(error) } })
+        })
+    }
+
+    const buyCards = (cardsToBuy) => {
+        getAccessTokenSilently().then(accessToken => {
+            GameService.buyCards(game.id, cardsToBuy, accessToken).then(response => {
                 dispatch({ type: 'game/clearSelectedCards' })
             }).catch(error => {
                 if (error.message === triggerBounceMessage) { return }
                 dispatch({ type: 'snackbar/message', payload: { type: 'error', message: parseError(error) } })
             })
+        }).catch(error => {
+            dispatch({ type: 'snackbar/message', payload: { type: 'error', message: parseError(error) } })
+        })
+    }
+
+    const chooseWorstCardToFollow = (myCards, suit) => {
+        const myCardsRich = cards.filter(card => myCards.indexOf(card.name) > -1)
+        const myTrumpCards = myCardsRich.filter(card => card.suit === suit || card.suit === "WILD")
+
+        if (myTrumpCards.length > 0) {
+            // Sort Ascending
+            myTrumpCards.sort((a, b) => a.value - b.value)
+            playCard(myTrumpCards[0].name)
+        } else {
+            // Sort Descending
+            myCardsRich.sort((a, b) => a.coldValue - b.coldValue)
+            playCard(myCardsRich[0].name)
         }
 
-        const processActions = () => {
+    }
 
-            // Deal when it's your turn
-            if (game.iamDealer && game.round.status === "CALLING" && game.cards.length === 0) {
-                setTimeout(() => deal(), 3000)
-            }
-            // Play card when you've pre-selected a card or you only have one left
-            else if (game.round.status === "PLAYING" && game.isMyGo) {
-                if (game.cards.length === 1) {
-                    playCard(game.cards[0])
-                }
-                else if (autoPlayCards.length > 0) {
-                    playCard(autoPlayCards[0].card)
-                }
-            }
-            // Buy cards in if you are the goer
-            else if (game.isMyGo && game.iamGoer && game.round.status === "BUYING") {
-                buyCards(game.cards)
-            }
+    const processActions = () => {
+
+        // Deal when it's your turn
+        if (game.iamDealer && game.round.status === "CALLING" && game.cards.length === 0) {
+            setTimeout(() => deal(), 3000)
         }
-
-        // If the game hasn't changed don't do anything
-        const currentGameHash = sha1(JSON.stringify(game) + JSON.stringify(autoPlayCards)).toString()
-
-        if (currentGameHash !== previousGameHash && game.status !== "FINISHED") {
-            updatePreviousGameHash(currentGameHash)
-            processActions()
+        // If in the bunker, Pass
+        else if (game.round.status === "CALLING" && game.me.score < -30) {
+            call(0)
         }
+        // Play card when you've pre-selected a card
+        else if (game.round.status === "PLAYING" && game.isMyGo && autoPlayCards.length > 0) {
+            playCard(autoPlayCards[0].card)
+        }
+        // Play card when you only have one left
+        else if (game.round.status === "PLAYING" && game.isMyGo && game.cards.length === 1) {
+            playCard(game.cards[0])
+        }
+        // Buy cards in if you are the goer
+        else if (game.isMyGo && game.iamGoer && game.round.status === "BUYING") {
+            buyCards(game.cards)
+        }
+        // Autoselect worst card if best card lead out
+        else if (game.round.status === "PLAYING" && game.isMyGo && bestCardLead(game.round)) {
+            chooseWorstCardToFollow(game.cards, game.round.suit)
+        }
+    }
 
-    }).catch(error => {
-        dispatch({ type: 'snackbar/message', payload: { type: 'error', message: parseError(error) } })
-    })
+    // If the game hasn't changed don't do anything
+    const currentGameHash = sha1(JSON.stringify(game) + JSON.stringify(autoPlayCards)).toString()
+
+    if (currentGameHash !== previousGameHash && game.status !== "FINISHED") {
+        updatePreviousGameHash(currentGameHash)
+        processActions()
+    }
 
     return null
 }
