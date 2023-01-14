a30d0008c652c93cbcac0bfedf07f2ff60248d68
Daithi Hearn <daithi.hearn@gmail.com>
2023-01-14 20:16:22 +0100

    Minor enhancements Adding sounds back in (currently not working) Moving play button to the left Adding pull to refresh

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useCallback } from "react"
 
 import { Card, CardGroup, CardHeader } from "reactstrap"
 
@@ -7,15 +7,34 @@ import StartNewGame from "../../components/StartNewGame/StartNewGame"
 import MyGames from "../../components/MyGames/MyGames"
 import GameStats from "../../components/GameStats/GameStats"
 import { withAuthenticationRequired } from "@auth0/auth0-react"
+import PullToRefresh from "react-simple-pull-to-refresh"
 
-import { useAppSelector } from "../../caches/hooks"
+import { useAppDispatch, useAppSelector } from "../../caches/hooks"
 import { getMyProfile } from "../../caches/MyProfileSlice"
+import GameService from "../../services/GameService"
+import { useSnackbar } from "notistack"
+import StatsService from "../../services/StatsService"
 
 const Home = () => {
+  const dispatch = useAppDispatch()
   const myProfile = useAppSelector(getMyProfile)
+  const { enqueueSnackbar } = useSnackbar()
+
+  const handleRefresh = useCallback(async () => {
+    if (myProfile.isAdmin)
+      dispatch(GameService.getAllPlayers()).catch((e: Error) =>
+        enqueueSnackbar(e.message, { variant: "error" })
+      )
+
+    dispatch(GameService.getAll())
+
+    dispatch(StatsService.gameStatsForPlayer()).catch((e: Error) =>
+      enqueueSnackbar(e.message, { variant: "error" })
+    )
+  }, [])
 
   return (
-    <>
+    <PullToRefresh onRefresh={handleRefresh}>
       <div className="app carpet">
         <DataLoader />
         <div className="game_wrap">
@@ -47,7 +66,7 @@ const Home = () => {
           </div>
         </div>
       </div>
-    </>
+    </PullToRefresh>
   )
 }
 
